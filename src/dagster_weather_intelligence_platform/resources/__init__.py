8c75696416d8f64ef7047f4365047eb1f7e32c9f
marcel <djambomarcellin@gmail.com>
2026-03-04 20:54:43 +0100

    chore: expose mlflow resource in package init

--- src/dagster_weather_intelligence_platform/resources/__init__.py
+++ src/dagster_weather_intelligence_platform/resources/__init__.py
@@ -1,5 +1,6 @@
 """Project resources package."""
 
 from dagster_weather_intelligence_platform.resources.gx import GreatExpectationsResource
+from dagster_weather_intelligence_platform.resources.mlflow_resource import mlflow_resource
 
-__all__ = ["GreatExpectationsResource"]
+__all__ = ["GreatExpectationsResource", "mlflow_resource"]

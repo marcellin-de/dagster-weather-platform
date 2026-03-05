408d05381fb2854d3effed3a614f032cf2dd2296
marcel <djambomarcellin@gmail.com>
2026-03-05 02:20:55 +0100

    chore: update dbt/weather_dbt/models/marts/mart_weather_daily.sql

--- dbt/weather_dbt/models/marts/mart_weather_daily.sql
+++ dbt/weather_dbt/models/marts/mart_weather_daily.sql
@@ -1,9 +1,15 @@
+{{ config(
+    materialized='table',
+    on_schema_change='fail',
+    contract={'enforced': true}
+) }}
+
 select
-  date_trunc('day', ts_utc) as day_utc,
-  avg(temperature_2m) as avg_temp_2m,
-  min(temperature_2m) as min_temp_2m,
-  max(temperature_2m) as max_temp_2m,
-  avg(wind_speed_10m) as avg_wind_10m,
-  sum(precipitation) as total_precipitation
+  cast(date_trunc('day', ts_utc) as timestamp) as day_utc,
+  cast(avg(temperature_2m) as double) as avg_temp_2m,
+  cast(min(temperature_2m) as double) as min_temp_2m,
+  cast(max(temperature_2m) as double) as max_temp_2m,
+  cast(avg(wind_speed_10m) as double) as avg_wind_10m,
+  cast(sum(precipitation) as double) as total_precipitation
 from {{ ref('stg_open_meteo_hourly') }}
 group by 1
